dee8b700b73b02ef6032f74283fdbb48531e82be
vanessaid27 <200200185@ese.ipsantarem.pt>
2022-03-30 12:34:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
-let laranjas;
+
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(0)
+	background(0);
 
 }
 
 function draw() {
 
+	background(0);
+	if(mouseY >= height/2) {
+    noFill();
+	}
+	else{
+		fill(230);
+	}
+	
 	stroke(230);
 	strokeWeight(2);
-	noFill();
 	circle(width / 2, height / 2, height / 4);
 
 
